0626b1c682775d1f013418bac910d0f627b111ea
Slashscreen <SlashScreen@users.noreply.github.com>
2023-08-03 20:07:43 -0700

    Now uses multithreading.
    Now takes 30 seconds instead of 30 minutes.

--- chunk_baker.gd
+++ chunk_baker.gd
@@ -1,13 +1,12 @@
 @tool
 class_name ChunkBaker
-extends NavigationRegion3D
+extends Node3D
 
 
 @export var navmesh_template:NavigationMesh
 @export_dir() var bake_path:String
 @export var terrain:Terrain3D
 @export_range(0, 100, 1, "or_greater") var bake_region:int
-var pos_offet:Vector3
 var region_done:bool
 var bundle:NavmeshBundle
 var region_offsets:Array[Vector2i]:
@@ -17,46 +16,31 @@ var region_size:int:
 	get:
 		return terrain.storage.region_size
 var stop_it:bool
-var bake_thread:Thread
+var width:int
+var height:int
+var area:int:
+	get:
+		return width * height
+var task_id:int
+var parsed_geometry:NavigationMeshSourceGeometryData3D
+var xz:Rect2
 
-signal bake_started
-signal bake_ended
+signal setup_finished
 
 
 ## Bakes a navigation mesh group for a [class Terrain3D], and any child nodes as usual. It wraps it in a [class NavmeshBundle] and saves it to disk.
 func bake_terrain() -> void:
-	print("Starting bake.")
-	#bake_started.emit()
-	print("Initializing bundles.")
-	bundle = NavmeshBundle.new()
+	_setup()
 	
-	if navigation_mesh == null:
-		navigation_mesh = navmesh_template.duplicate()
+	print("Setting up worker pool")
+	task_id = WorkerThreadPool.add_group_task(_work, area)
+	WorkerThreadPool.wait_for_group_task_completion(task_id)
 	
-	print("Processing region.")
-	#bake_thread = Thread.new()
-	#bake_thread.start(_loop_thread.bind())
-	_loop_thread()
-	# Save bundle to disk.
-
-
-func _scan() -> void:
-	print("duplicating mesh")
-	navigation_mesh = navmesh_template.duplicate()
-	print("Changing AABB")
-	var aabb:AABB = navigation_mesh.filter_baking_aabb
-	navigation_mesh.filter_baking_aabb = aabb.grow(navmesh_template.agent_radius) # grow to get rid of edge
-	navigation_mesh.filter_baking_aabb_offset = pos_offet
-	print("Baking chunk...")
-	bake_navigation_mesh()
-	print("Chunk finished.")
-	await bake_finished
+	_teardown()
 
 
 func stop() -> void:
 	stop_it = true
-	bake_thread.wait_to_finish()
-	stop_it = false
 
 
 func _wrap_up() -> void:
@@ -64,45 +48,53 @@ func _wrap_up() -> void:
 	print("Finished baking.")
 
 
-func _loop_thread() -> void:
+func _setup() -> void:
+	print("Setting up worker.")
 	var offsets = region_offsets
 	var region_uv = offsets[bake_region]
 	# Grab region info
 	var region_center:Vector2 = region_uv*region_size
 	#var region_index = terrain.storage.get_region_index(Vector3(region_center.x, 0, region_center.y))
-	var xz:Rect2 = Rect2(region_center.x - region_size*.5, region_center.y - region_size*.5, region_size, region_size)
+	xz = Rect2(region_center.x - region_size*.5, region_center.y - region_size*.5, region_size, region_size)
 	var aabb:AABB = navmesh_template.filter_baking_aabb
 	
-	#print("Processing region %s of %s." % [region_index, offsets.size()])
-	var total = range(xz.position.x, xz.end.x, aabb.size.x).size() * range(xz.position.y, xz.end.y, aabb.size.z).size() # can i not do math
-	var i = 1
-	# Bake each chunk.
-	for x in range(xz.position.x, xz.end.x, aabb.size.x):
-		for y in range(xz.position.y, xz.end.y, aabb.size.z):
-			if stop_it:
-				bake_ended.emit()
-				return
-			
-			pos_offet = Vector3(x, 0, y)
-			print(pos_offet)
-			
-			print("duplicating mesh")
-			navigation_mesh = navmesh_template.duplicate()
-			print("Changing AABB")
-			var agent_diameter = navmesh_template.agent_radius * 2
-			navigation_mesh.filter_baking_aabb.size = (navigation_mesh.filter_baking_aabb as AABB).size + Vector3(agent_diameter, 0, agent_diameter)
-			print((navigation_mesh.filter_baking_aabb as AABB).size + Vector3(agent_diameter, 0, agent_diameter))
-			print((navigation_mesh.filter_baking_aabb as AABB).size)
-			navigation_mesh.filter_baking_aabb_offset = pos_offet
-			print("Baking chunk...")
-			bake_navigation_mesh()
-			print("Chunk finished.")
-			await bake_finished
-			
-			#ResourceSaver.save(navigation_mesh, "%s/%s.tres" % [bake_path, pos_offet])
-			bundle.meshes.append(navigation_mesh)
-			
-			print("Region Progress: %s / %s" % [i, total])
-			i += 1
+	bundle = NavmeshBundle.new()
+	width = range(xz.position.x, xz.end.x, aabb.size.x).size() # may need to -1
+	height = range(xz.position.y, xz.end.y, aabb.size.z).size()
+	# Fill bundle
+	for _i in range(area):
+		bundle.meshes.append(navmesh_template.duplicate())
+	# parse geometry
+	var nm:NavigationMesh = navmesh_template.duplicate()
+	nm.filter_baking_aabb = AABB()
+	nm.filter_baking_aabb_offset = Vector3.ZERO
+	print("Parsing geometry...")
+	parsed_geometry = NavigationMeshSourceGeometryData3D.new()
+	NavigationMeshGenerator.parse_source_geometry_data(nm, parsed_geometry, self, func(): print("Parsed geometry."))
+	print("Finished setup.")
+
+
+func _teardown() -> void:
+	print("Tearing down.")
+	ResourceSaver.save(bundle, "%s/%s_%s_bundle.tres" % [bake_path, name, Time.get_unix_time_from_system()])
+	bundle = null
+
+func _work(index:int) -> void:
+	var size = (navmesh_template.filter_baking_aabb as AABB).size
+	var pos_offset = Vector3(_get_x(index % width), 0, _get_y(floori(index / height)))
+	print(pos_offset)
 	
-	_wrap_up()
+	var agent_diameter = navmesh_template.agent_radius * 2
+	bundle.meshes[index].filter_baking_aabb.size = size + Vector3(agent_diameter, 0, agent_diameter) # expand to compensate for agent diameter
+	bundle.meshes[index].filter_baking_aabb_offset = pos_offset
+	
+	print("Baking chunk %s..." % index)
+	NavigationMeshGenerator.bake_from_source_geometry_data(bundle.meshes[index], parsed_geometry, func(): print("Chunk %s finished." % index))
+
+
+func _get_x(x:int) -> float:
+	return lerp(xz.position.x, xz.end.x, x as float / width as float)
+
+
+func _get_y(y:int) -> float:
+	return lerp(xz.position.y, xz.end.y, y as float / height as float)
--- nav_baker_plugin.gd
+++ nav_baker_plugin.gd
@@ -16,7 +16,7 @@ func _parse_begin(object) -> void:
 	var stop_bake:Button = Button.new()
 	stop_bake.text = "Stop bake"
 	stop_bake.pressed.connect(func(): (object as ChunkBaker).stop_it = true)
-	(object as ChunkBaker).bake_started.connect(func(): stop_bake.disabled = false)
-	(object as ChunkBaker).bake_ended.connect(func(): stop_bake.disabled = true)
+	#(object as ChunkBaker).bake_started.connect(func(): stop_bake.disabled = false)
+	#(object as ChunkBaker).bake_ended.connect(func(): stop_bake.disabled = true)
 	#stop_bake.disabled = true
 	add_custom_control(stop_bake)
